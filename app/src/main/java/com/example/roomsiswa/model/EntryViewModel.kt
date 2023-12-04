62b829056167b73c21642a5f9b2f3ba6a3d833ec
hariwr <hariwr25@gmail.com>
2023-12-04 11:23:33 +0700

    Membuat Class EntryViewModel

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.roomsiswa.model
 
+import android.view.View
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.OfflineRepositoriSiswa
+import com.example.roomsiswa.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+}
