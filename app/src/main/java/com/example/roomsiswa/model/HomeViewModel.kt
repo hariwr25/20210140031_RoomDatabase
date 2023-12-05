181cb5d3e5aa8813e38d0223276a8e9aa4bbbc9c
hariwr <hariwr25@gmail.com>
2023-12-05 09:45:29 +0700

    Add class HomeViewModel

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,4 +1,10 @@
 package com.example.roomsiswa.model
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.RepositoriSiswa
+
+class HomeViewModel (private val repositoriSiswa: RepositoriSiswa):ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
