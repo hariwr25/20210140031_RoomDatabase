05b1b7250b8e1e2c0bdce7a109e1253d45f7b559
hariwr <hariwr25@gmail.com>
2023-12-06 10:55:33 +0700

    Add EntrySiswaBody

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,21 +1,28 @@
 package com.example.roomsiswa.ui.halaman
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.model.EntryViewModel
 import com.example.roomsiswa.model.PenyediaViewModel
+import com.example.roomsiswa.model.UIStateSiswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
@@ -58,3 +65,29 @@ fun EntrySiswaScreen(
         )
     }
 }
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column {
+        verticalArrangment = Arrangment.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            FormInputSiswa(
+                detailSiswa = uiStateSiswa.detailSiswa,
+                onValueChange = onSiswaValueChange,
+                modifier = Modifier.fillMaxWidth()
+            )
+        onClick = onSaveClick,
+        enabled = uiStateSiswa.isEntryValid,
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier.fillMaxWidth()
+            ) {
+           Text(stringResource(R.string.btn_submit))
+    }
+    }
+    }
+    }
