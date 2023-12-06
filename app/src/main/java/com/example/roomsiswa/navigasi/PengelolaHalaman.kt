abbe40dea0b5a49d7215bfaf453d8f691545053b
hariwr <hariwr25@gmail.com>
2023-12-06 14:37:27 +0700

    Menambahkan fun SiswaApp

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,10 @@
 package com.example.roomsiswa.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
