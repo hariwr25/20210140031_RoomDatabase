95ac8c78c479e571e346eba056240c1e1c560ed0
hariwr <hariwr25@gmail.com>
2023-12-06 15:06:02 +0700

    Edit PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.roomsiswa.model
 
-import android.app.Application
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,14 +8,19 @@ import com.example.roomsiswa.AplikasiSiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
+
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
     }
+
+    fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 }
 
-fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
